ccc6f89db2886d2a96a4fb7126ea3e5611c541c0
Luis Martinez <luismartinez.rodriguez.1@gmail.com>
2022-08-12 13:42:25 +0200

    Ejercicio 12 completado

---
+++ js12/main.js
@@ -0,0 +1,12 @@
+const array = ["a", "b", "c", "d", "e"];
+array.forEach(item => {
+    console.log(item);
+})
+
+let numero = 5;
+while (numero > 0) {
+    numero--;
+    if (numero === 0) {
+        console.log(numero)
+    }
+}
