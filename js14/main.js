9272bfc2a32084841c100c5b4285d8be2438f8da
Luis Martinez <luismartinez.rodriguez.1@gmail.com>
2022-08-29 13:33:24 +0200

    Ejercicio 14 terminado

--- js14/main.js
+++ js14/main.js
@@ -4,10 +4,10 @@ console.log(arr.fill("🍺",3));
 const piña = (currentValue) => currentValue === "🍍";
 const pizza = (currentValue) => currentValue === "🍕";
 const arr1=["🍕","🍕","🍍","🍕","🍕"]
-console.log(arr1.some(piña))
-console.log(arr1.filter(pizza))
+console.log(arr1.some(piña));
+console.log(arr1.filter(pizza));
 
-const arr2=["🍓","🍋","🍓","🍋","🍓"]
+const arr2=["🍓","🍋","🍓","🍋","🍓"];
 arr2.map(item => {
     switch(item) {
         case "🍓":
@@ -22,5 +22,20 @@ arr2.map(item => {
     }
 });
 
-const arr3=["🌶️","🥛","🌶️","🥛","🌶️","🥛"]
-console.log(arr3.)
+const arr3=["🌶️","🥛","🌶️","🥛","🌶️","🥛"];
+for (let i=0; i<arr3.length; i++) {
+    if (arr3[i] == "🌶️") {
+        arr3.splice(i+1, 0, '🥵');
+    }
+}
+console.log(arr3);
+
+const arr4=["🎴","🎴","🎴","🃏","🎴","🎴","🎴"]
+for (let x=0; x<arr4.length-1; x++) {
+    if (arr4[x] == "🎴") {
+        arr4.splice(x+1,0,"🃏");
+    } else if (arr4[6]=="🃏") {
+        arr4.splice(5,1)
+    }
+}
+console.log(arr4);
