c26a1fb108cb48a34b5bdf7968269056e9d070dd
Luis Martinez <luismartinez.rodriguez.1@gmail.com>
2022-08-29 10:31:18 +0200

    Modificación

--- js9/main.js
+++ js9/main.js
@@ -1,6 +1,8 @@
 // Math.round es redondear a los números enteros y Math.random es un numero aleatorio del 0 al 1;
 console.log(Math.round(Math.random()));
 
-// Hay un código extenso pero que no entiendo a este nivel en la página developer.mozilla.org. Por lo tanto he encontrado esta forma de solucionarlo ya que es más simple: el floor te redondea al entero por lo tanto multiplicas, redondeas y divides para redondear al fin y al cabo;
+/* Hay un código extenso pero que no entiendo a este nivel en la página developer.mozilla.org. 
+Por lo tanto he encontrado esta forma de solucionarlo ya que es más simple: el floor te redondea al entero 
+por lo tanto multiplicas, redondeas y divides para redondear al fin y al cabo; */
 const n = Math.floor(Math.PI*1000) / 1000;
 console.log(n);
