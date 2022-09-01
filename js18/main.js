c907a59a3128e225913706bfd791e9588d8a3f54
Luis Martinez <luismartinez.rodriguez.1@gmail.com>
2022-09-01 12:43:46 +0200

    Ejercicio 18 terminado

--- js18/main.js
+++ js18/main.js
@@ -1,1 +1,16 @@
-/* Usar el jQuery.hover (Diapo 185)*/
+const img1 =document.getElementById("img-1");
+const img2 =document.getElementById("img-2");
+function front1() {
+    img1.innerHTML='<img onmouseout="back1()" class="img-fluid img-thumbnail" style="height:58.3vmin; width: 40vmin;" src="./card-front.png" alt="Carta">'
+}
+
+function back1() {
+    img1.innerHTML='<img id="img1" onmouseover="front1()" class="img-fluid img-thumbnail" style="height:58.3vmin; width: 40vmin;" src="./card-back.png" alt="Carta">'
+}
+function front2() {
+    img2.innerHTML='<img onmouseout="back2()" class="img-fluid img-thumbnail" style="height:58.3vmin; width: 40vmin;" src="./card-front.png" alt="Carta">'
+}
+
+function back2() {
+    img2.innerHTML='<img id="img1" onmouseover="front2()" class="img-fluid img-thumbnail" style="height:58.3vmin; width: 40vmin;" src="./card-back.png" alt="Carta">'
+}
